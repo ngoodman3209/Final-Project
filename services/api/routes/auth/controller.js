03702226c7fe8379932134393da2727127e13f47
Nick <ngoodman3209@gmail.com>
2018-07-31 20:33:52 -0600

    removed previous links to git hub

--- services/api/routes/auth/controller.js
+++ services/api/routes/auth/controller.js
@@ -9,8 +9,8 @@ export default class AuthController {
   async login(req, res) {
     //Remove the temporary logic below and hook up the command
     const { email, password } = req.body
-    if (email === 'bryce@helio.com' && password === '123')
-      res.send({ username: 'quietome', email: 'bryce@helio.com', password: '123' })
+    if (email === 'me@helio.com' && password === '123')
+      res.send({ username: 'me', email: 'me@helio.com', password: '123' })
     else
       throw new Error('unauthorized user')
   }
